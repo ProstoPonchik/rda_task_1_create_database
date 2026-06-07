436f3f8bd79bd768a4d9e291dcc51037de280190
ProstoPonchik <vlad.vislovukh@gmail.com>
2026-06-07 21:02:22 +0200

    Solved!

--- task.sql
+++ task.sql
@@ -1,1 +1,37 @@
-# Write your SQL code for the database creation here. Good luck! 
+create database ShopDB;
+use ShopDB;
+
+create table Products (
+	ID int auto_increment,
+    Name varchar(50),
+    Description varchar(100),
+    Price int,
+    WarehouseAmount int,
+    primary key (ID)
+);
+
+create table Customers (
+	ID int auto_increment,
+    FirstName varchar(50),
+    LastName varchar(50),
+    Email varchar(50),
+    Address varchar(100),
+    primary key (ID)
+);
+
+create table Orders (
+	ID int auto_increment,
+    CustomerID int,
+    Date date,
+    primary key (ID),
+    foreign key (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+);
+
+create table OrderItems (
+	ID int auto_increment,
+    OrderID int,
+    ProductID int,
+    primary key (ID),
+	foreign key (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    foreign key (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
